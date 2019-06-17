7b820229aeb53e816d35e8dcf3a877ad551bba58
meghakailas <meghakailas.sayonetech@gmail.com>
2019-06-17 14:50:53 +0530

    shopping website

---
+++ SAYONETASKS/website/jabong-js.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.next').on('click', function(){
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
+
+    if(nextImg.length){
+      currentImg.removeClass('active').css('z-index', -10);
+      nextImg.addClass('active').css('z-index', 10);
+    }
+  });
+
+  $('.prev').on('click', function(){
+    var currentImg = $('.active');
+    var prevImg = currentImg.prev();
+
+    if(prevImg.length){
+      currentImg.removeClass('active').css('z-index', -10);
+      prevImg.addClass('active').css('z-index', 10);
+    }
+  });
+});
+
+
+function loginform() {
+                          document.getElementById("myloginform").style.display = "block";
+                        }                       
+                        function loginclose() {
+                          document.getElementById("myloginform").style.display = "none";
+                        }
